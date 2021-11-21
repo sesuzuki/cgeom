9a925e457897ca50b8a15432ab2ede6e9d8c4077
sesuzuki-gcm <80114464+sesuzuki-gcm@users.noreply.github.com>
2021-11-21 02:55:51 +0100

    "Initial Commit"

--- build/3rdparty/libigl/embree/common/algorithms/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/suzuki/Code/cgeom/3rdparty/libigl/external/embree/common/algorithms
-# Build directory: /Users/suzuki/Code/cgeom/build/3rdparty/libigl/embree/common/algorithms
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/3rdparty/libigl/embree/common/sys/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/suzuki/Code/cgeom/3rdparty/libigl/external/embree/common/sys
-# Build directory: /Users/suzuki/Code/cgeom/build/3rdparty/libigl/embree/common/sys
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
